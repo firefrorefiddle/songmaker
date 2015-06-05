d5ad1d7033a83b419487d9faff2e71b1d1627f2e
Michael Hartl <mikehartl17@gmail.com>
2015-06-05 12:11:37 +0200

    accept colons in header fields

--- src/SongMaker/Read/Header.hs
+++ src/SongMaker/Read/Header.hs
@@ -20,19 +20,22 @@ knownHeaders = ["title",
                 "extra-title-index",
                 "numbering"]
 
-splitEach :: (Eq a) => a -> [a] -> [[a]]
+{-splitEach :: (Eq a) => a -> [a] -> [[a]]
 splitEach c l = case break (== c) l of
   (l, []) -> [l]
   (l, r) -> l:splitEach c (dropWhile (== c) r)
+-}
 
-tryReadHeaderLine l = case splitEach ':' l of 
-  [name, value] ->
-    let value' = dropWhile isSpace . reverse .
-                 dropWhile (not . isAlphaNum) . reverse $ value
-    in if length (words name) == 1
-       then return (head.words $ name, value')
-       else fail "invalid header format"
-  _ -> fail "invalid header format"
+tryReadHeaderLine l =
+  let (name, value) = break (==':') l
+      value' = dropWhile isSpace . reverse .
+               dropWhile (not . isAlphaNum) . reverse .
+               dropWhile (== ':') $ value
+  in if null value'
+     then fail "empty header value"
+     else if length (words name) == 1
+          then return (head.words $ name, value')
+          else fail "invalid header format"
                         
 isHeaderLine = isJust . tryReadHeaderLine
 readHeaderLine = fromJust . tryReadHeaderLine
