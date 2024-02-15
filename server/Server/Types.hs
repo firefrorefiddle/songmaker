23c46e5872923bc3569cfa0992fc0aad153536f1
Mike Hartl <mikehartl17@gmail.com>
2024-02-15 17:33:06 +0100

    Some adjustments

--- server/Server/Types.hs
+++ server/Server/Types.hs
@@ -1,22 +1,54 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
 module Server.Types where
 
 import Relude
 
+import Data.Aeson
 import Data.Text (Text)
+import Data.Time.Clock
 
 type Author = Text
+newtype Username = Username {unUserName :: Text}
+    deriving (Read, Show, Eq, Generic, FromJSON, ToJSON)
 
 data Song = Song
     { author :: Author
     , title :: Text
     , content :: Text
+    , enteredBy :: Username
     }
-    deriving (Read, Show, Eq, Generic)
+    deriving (Read, Show, Eq, Generic, FromJSON, ToJSON)
+
+data SongVersion = SongVersion
+    { song :: Song
+    , enteredBy :: Username
+    , designation :: Text
+    , timeStamp :: UTCTime
+    , content :: Text
+    , userComments :: Text
+    }
+    deriving (Read, Show, Eq, Generic, FromJSON, ToJSON)
 
 data SongbookContent = SBSong Song | SBNumbering Int
-    deriving (Read, Show, Eq, Generic)
+    deriving (Read, Show, Eq, Generic, FromJSON, ToJSON)
 
-data SongBook = SongBook
+newtype SongBook = SongBook
     { title :: Text
-    , songs :: [Song]
     }
+    deriving (Read, Show, Eq, Generic, FromJSON, ToJSON)
+
+data SongBookVersion = SongBookVersion
+    { songBook :: SongBook
+    , designation :: Text
+    , enteredBy :: Username
+    , contents :: [SongbookContent]
+    }
+    deriving (Read, Show, Eq, Generic, FromJSON, ToJSON)
+
+data User = User
+    { username :: Text
+    , firstName :: Text
+    , lastName :: Text
+    }
+    deriving (Read, Show, Eq, Generic, FromJSON, ToJSON)
